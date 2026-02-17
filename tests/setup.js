097025edda6ee95e4caa45cc1e0c621702a49374
DeVelezper <develezper@gmail.com>
2026-02-17 12:02:25 -0500

    chore: reset main for redesign

--- tests/setup.js
+++
@@ -1,8 +0,0 @@
-import { beforeEach } from "vitest";
-
-beforeEach(() => {
-  window.localStorage.clear();
-  window.sessionStorage.clear();
-  window.location.hash = "#/";
-  document.body.innerHTML = '<div id="toast-container"></div><main id="main"></main><nav id="navbar"></nav>';
-});
